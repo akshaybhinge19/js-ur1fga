6064edb0395820db17637f2ca075fa34803d7201
akshaybhinge19 <akshaybhinge19@gmail.com>
2021-07-29 00:16:28 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
 // Import stylesheets
 import './style.css';
-document.getElementById("child1").addEventListener("mouseover", colorChange1);
-document.getElementById("child1").addEventListener("mouseout", colorFade1);
-function colorChange1(){  
-  document.getElementById("child1").style.backgroundColor='red';  
+
+let childCss = document.getElementById('child2');
+
+let isStarted = false;
+let start = document.getElementById('gameStart');
+start.onclick = function() {
+  game();
+};
+function game() {
+  isStarted = true;
 }
-function colorFade1(){  
-  document.getElementById("child1").style.backgroundColor='white';  
+childCss.addEventListener('mouseover', colorChange2);
+childCss.addEventListener('mouseout', colorFade2);
+function colorChange2() {
+  if (!isStarted) return;
+  childCss.style.backgroundColor = 'pink';
 }
-document.getElementById("child2").addEventListener("mouseover", colorChange2);
-document.getElementById("child2").addEventListener("mouseout", colorFade2);
-function colorChange2(){  
-  document.getElementById("child2").style.backgroundColor='blue';
-  document.getElementById("line1").style.visibility='visible';
-  document.getElementById("line2").style.visibility='visible';
+function colorFade2() {
+  if (!isStarted) return;
+  childCss.style.backgroundColor = 'Transparent';
 }
-function colorFade2(){  
-  document.getElementById("child2").style.backgroundColor='white';
-  document.getElementById("line1").style.visibility='hidden';
-  document.getElementById("line2").style.visibility='hidden';
+
+document.getElementById('parent').addEventListener('click', getRandomInt);
+var count = 0;
+let level = 1;
+function getRandomInt() {
+  if (!isStarted) return;
+  var a = Math.floor(Math.random() * 410 + 20);
+  var b = Math.floor(Math.random() * 410 + 20);
+  if (count == 0) {
+    document.getElementById('count').innerHTML =
+      'Hearts Collected' + '<br>' + count;
+  }
+  childCss.style.top = a + 'px';
+  childCss.style.left = b + 'px';
 }
+childCss.onclick = function() {
+  getCount();
+};
+function getCount() {
+  if (!isStarted) return;
+  count++;
+  document.getElementById('count').innerHTML =
+    'Hearts Collected' + '<br>' + count;
+  if (count == 10) {
+    alert('Winner of Hearts');
+    alert('completed Level' + level);
+    document.getElementById('level').innerHTML = 'Level' + ' - ' + ++level;
+    count = 0;
+  }
+  if (level == 10) {
+    alert('Congratulations! You are the King of Hearts');
+    level = 0;
+  }
+}
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
